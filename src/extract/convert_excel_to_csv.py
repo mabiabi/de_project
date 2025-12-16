226f554eb428f693bc04dfec8ecd64eae5601b78
mabiabi <158147209+mabiabi@users.noreply.github.com>
2025-12-16 12:00:42 +0000

    convert

---
+++ src/extract/convert_excel_to_csv.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+
+def convert_excel_to_csv(excel_path, output_path):
+    print(f"Reading Excel file: {excel_path}")
+    df = pd.read_excel(excel_path)  # requires openpyxl (already installed)
+    
+    print(f"Saving CSV file: {output_path}")
+    df.to_csv(output_path, index=False)
+
+if name == "__main__":
+    excel_file = "/workspaces/de_project_1/data/raw/Online Retail.xlsx"
+    csv_file = "/workspaces/de_project_1/data/raw/online_retail.csv"
+
+    convert_excel_to_csv(excel_file, csv_file)
+    print("✔ Conversion complete.")
