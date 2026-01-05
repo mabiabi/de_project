6178a75eb3bb9a9504b9f4d8d1a0fba7c896b090
mabiabi <158147209+mabiabi@users.noreply.github.com>
2026-01-05 18:20:44 +0000

    first etl code

---
+++ src/load/build_silver_sales.py
@@ -0,0 +1,29 @@
+from src.extract.read_sales_csv_chunked import read_sales_csv_in_chunks
+from src.transform.clean_sales_chunk import clean_sales_chunk
+from pathlib import Path
+
+OUTPUT_PATH = Path("data/processed/sales_silver.csv")
+
+def build_silver_layer():
+    first_write = True
+
+    for chunk in read_sales_csv_in_chunks(
+        "data/raw/online_retail.csv",
+        chunk_size=100_000
+    ):
+        cleaned = clean_sales_chunk(chunk)
+
+        cleaned.to_csv(
+            OUTPUT_PATH,
+            mode="w" if first_write else "a",
+            header=first_write,
+            index=False
+        )
+
+        first_write = False
+
+        print(f"Written {len(cleaned):,} rows to silver layer")
+        
+
+if __name__ == "__main__":
+    build_silver_layer()
